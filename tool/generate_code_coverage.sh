f6675ca33f6a57a6267a735800e63de8c1b8355f
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-02-17 21:26:35 -0500

    Change min code coverage (for now)

--- tool/generate_code_coverage.sh
+++ tool/generate_code_coverage.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env bash
 set -e
 set -o pipefail
 
 # Required to get coverage in lcov form
 dart pub global activate coverage
 
+# Required to remove certain files from coverage
+dart pub global activate remove_from_coverage
+
 # Run tests and generate lcov
-dart test --test-randomize-ordering-seed=random --coverage=coverage && dart pub global run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
+dart test --coverage=coverage && pub run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
+
+# Don't report coverage on `.g.` and `.freezed.` files
+pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.freezed.dart$'
+pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.g.dart$'

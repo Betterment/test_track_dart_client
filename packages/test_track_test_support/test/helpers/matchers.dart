bda2866c866053407ce974fbbd00516ff2ef48f2
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-02-18 22:05:43 -0500

    Complete tests for FakeAnalyticsProvider

---
+++ packages/test_track_test_support/test/helpers/matchers.dart
@@ -0,0 +1,55 @@
+import 'package:test/expect.dart';
+
+/// Returns a matcher that matches if the match argument *only*
+/// contains the expect items
+Matcher onlyContains<T>(List<T> expected) {
+  return _OnlyContains(expected);
+}
+
+class _OnlyContains<T> extends Matcher {
+  final List<T> _expected;
+
+  _OnlyContains(this._expected);
+
+  @override
+  Description describe(Description description) =>
+      description.add('only ').addDescriptionOf(_expected);
+
+  @override
+  Description describeMismatch(
+    item,
+    Description mismatchDescription,
+    Map matchState,
+    bool verbose,
+  ) {
+    if (item is List<T>) {
+      final items = [...item];
+      if (items.length != _expected.length) {
+        final diff = (items.length - _expected.length).abs();
+        mismatchDescription.add('differed in length by $diff items');
+      }
+      return super.describeMismatch(
+        item,
+        mismatchDescription,
+        matchState,
+        verbose,
+      );
+    } else {
+      return mismatchDescription.add('was not a list');
+    }
+  }
+
+  @override
+  bool matches(item, Map matchState) {
+    if (item is List<T>) {
+      final items = [...item];
+      if (items.length != _expected.length) {
+        return false;
+      }
+      items.removeWhere((i) => _expected.contains(i));
+      return items.isEmpty;
+    } else {
+      return false;
+    }
+  }
+}
--- packages/test_track_test_support/test/src/fake_analytics_provider_test.dart
+++ packages/test_track_test_support/test/src/fake_analytics_provider_test.dart
@@ -1,6 +1,8 @@
 import 'package:test/test.dart';
 import 'package:test_track_test_support/test_track_test_support.dart';
 
+import '../helpers/matchers.dart';
+
 void main() {
   group('FakeAnalyticsProvider', () {
     late FakeAnalyticsProvider subject;
@@ -22,10 +24,10 @@ void main() {
         );
         expect(
           subject.assignmentsTracked,
-          [
+          onlyContains([
             Pair('123', assignment1),
             Pair('456', assignment2),
-          ],
+          ]),
         );
       });
     });
@@ -33,18 +35,21 @@ void main() {
     group('tracked', () {
       test('it returns true if the assignment was tracked', () {
         final assignment1 = AssignmentFactory.build();
+
         subject.trackAssignment(
           visitorId: '123',
           assignment: assignment1,
         );
+
         expect(
           subject.tracked(assignment: Pair('123', assignment1)),
           isTrue,
         );
       });
 
-      test('it false true if the assignment was not tracked', () {
+      test('it returns false if the assignment was not tracked', () {
         final assignment1 = AssignmentFactory.build();
+
         expect(
           subject.tracked(assignment: Pair('123', assignment1)),
           isFalse,
@@ -68,6 +73,56 @@ void main() {
         );
         expect(
           subject.noAssignmentsTracked(),
+          isFalse,
+        );
+      });
+    });
+
+    group('visitorsIdentified', () {
+      test('it collects identified visitors', () {
+        subject.identify(visitorId: '123');
+        subject.identify(visitorId: '456');
+
+        expect(
+          subject.visitorsIdentified,
+          onlyContains(['123', '456']),
+        );
+      });
+    });
+
+    group('identified', () {
+      test('it returns true if the visitor was identified', () {
+        subject.identify(visitorId: '123');
+
+        expect(
+          subject.identified(visitorId: '123'),
+          isTrue,
+        );
+      });
+
+      test('it returns false if the visitor was not identified', () {
+        subject.identify(visitorId: '123');
+
+        expect(
+          subject.identified(visitorId: '456'),
+          isFalse,
+        );
+      });
+    });
+
+    group('noVisitorsIdentified', () {
+      test('it returns true if no visitories were identified', () {
+        expect(
+          subject.noVisitorsIdentified(),
+          isTrue,
+        );
+      });
+
+      test('it returns false if any visitories were identified', () {
+        subject.identify(visitorId: '123');
+
+        expect(
+          subject.noVisitorsIdentified(),
           isFalse,
         );
       });

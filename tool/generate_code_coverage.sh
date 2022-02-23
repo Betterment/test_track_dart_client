bce517985d551af6c9de1622dc57b28763d618d2
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-02-23 09:56:46 -0500

    More granular lcov naming

--- tool/generate_code_coverage.sh
+++ tool/generate_code_coverage.sh
@@ -1,6 +1,12 @@
 set -e
 set -o pipefail
 
+if [ -z "$1" ]
+  then
+    echo "Must include coverage file name"
+    exit 1
+fi
+
 # Required to get coverage in lcov form
 dart pub global activate coverage
 
@@ -11,4 +17,4 @@ dart pub global activate remove_from_coverage
 dart test --coverage=coverage && pub run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
 
 # Don't report coverage on `.g.` and `.freezed.` files
-dart pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.freezed.dart$' -r '.g.dart$' -r '^.*/factories/.*_factory.dart$' -r '.*/helpers/pair.dart$'
+dart pub global run remove_from_coverage:remove_from_coverage -f coverage/$1 -r '.freezed.dart$' -r '.g.dart$' -r '^.*/factories/.*_factory.dart$' -r '.*/helpers/pair.dart$'

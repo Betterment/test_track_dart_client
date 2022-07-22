0d13438d75455bf58a4e2e625970c2e2f229591e
Brandon Trautmann <btrautmann@users.noreply.github.com>
2022-07-22 09:42:37 -0400

    fix: don't run pub directly

--- tool/generate_code_coverage.sh
+++ tool/generate_code_coverage.sh
@@ -8,7 +8,7 @@ dart pub global activate coverage
 dart pub global activate remove_from_coverage
 
 # Run tests and generate lcov
-dart test --coverage=coverage && pub run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
+dart test --coverage=coverage && dart pub run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
 
 # Don't report coverage on `.g.` and `.freezed.` files
 dart pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '.freezed.dart$' -r '.g.dart$' -r '^.*/factories/.*_factory.dart$' -r '.*/helpers/pair.dart$'

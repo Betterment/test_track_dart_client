8c74905150d7007f14827f1203771038e03e3ddf
Brandon Trautmann <btrautmann@users.noreply.github.com>
2023-02-23 11:46:07 -0500

    make headers static const

--- packages/test_track/lib/src/networking/interceptors/required_headers_interceptor.dart
+++ packages/test_track/lib/src/networking/interceptors/required_headers_interceptor.dart
@@ -3,16 +3,16 @@ import 'package:dio/dio.dart';
 /// An [Interceptor] that adds headers required to communicate with TestTrack to each
 /// request.
 class RequiredHeadersInterceptor extends Interceptor {
+  static const _requiredHeaders = <String, String>{
+    Headers.acceptHeader: 'application/json; charset=utf-8',
+    Headers.contentTypeHeader: 'application/json; charset=utf-8',
+  };
   @override
   Future onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    const requiredHeaders = <String, String>{
-      Headers.acceptHeader: 'application/json; charset=utf-8',
-      Headers.contentTypeHeader: 'application/json; charset=utf-8',
-    };
     return super.onRequest(
       options.copyWith(
-        headers: options.headers..addAll(requiredHeaders),
+        headers: options.headers..addAll(_requiredHeaders),
       ),
       handler,
     );

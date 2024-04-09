10b1d300b585f3bebf40ed29d436865e6efbfe96
Claire Davis <claire.davis@betterment.com>
2024-04-09 16:34:31 -0400

    upgrade dependencies

--- packages/test_track/lib/src/models/identifier.freezed.dart
+++ packages/test_track/lib/src/models/identifier.freezed.dart
@@ -12,7 +12,7 @@ part of 'identifier.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 Identifier _$IdentifierFromJson(Map<String, dynamic> json) {
   return _Identifier.fromJson(json);
@@ -124,7 +124,7 @@ class _$IdentifierImpl implements _Identifier {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$IdentifierImpl &&
